d91978462112c723cf0b052ca26d077fa2f0c504
Hecomer <yan.lyandin111@yandex.ru>
2024-04-07 01:29:45 +0500

    added some data to be sure

--- tests.py
+++ tests.py
@@ -1,6 +1,8 @@
 from database import Category, get_session
 
 session = get_session()
+new_category = Category(title="Rockstar")
+session.add(new_category)
 categories = session.query(Category).all()
 print(categories)
 session.commit()
