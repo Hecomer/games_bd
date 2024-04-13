fb59bb1372abc32ae4beeb194ec1e7ef556ba9b2
kobel <kobelev-kir@mail.ru>
2024-04-13 16:13:17 +0500

    added store window

--- ui/ui_create_user.py
+++ ui/ui_create_user.py
@@ -88,7 +88,7 @@ if __name__ == "__main__":
     import sys
     app = QtWidgets.QApplication(sys.argv)
     Form = QtWidgets.QWidget()
-    ui = Ui_Form()
+    ui = Ui_CreateNewUser()
     ui.setupUi(Form)
     Form.show()
     sys.exit(app.exec_())
